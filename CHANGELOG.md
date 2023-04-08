052cadb7583577d170da8a7cc51436aeb317c84c
Bast-94 <bh.69@live.fr>
2023-04-08 17:38:54 +0200

    ADD(generate_blockchain_by_actor).

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 
+# v1.1.0
+- Add:
+    - `generate_blockchain_by_actor` in order to get and clean blochain_by_actor time series.
+
 # v1.0.0 
 - Add:
     - `generate_external_time_series` in order to get and clean external time series.
--- scripts/data_frame_generator.py
+++ scripts/data_frame_generator.py
@@ -3,7 +3,10 @@ import pandas as pd
 def generate_external_time_series(file_path, window=1) -> pd.DataFrame:
     # Load Dataframe 
     df = pd.read_csv(file_path)
+    
+    # Unuseful columns
     df = df.drop(['Unnamed: 0'],axis=1)
+    
     df['date'] = pd.to_datetime(df['date'])
     df['HashRate'] = df['HashRate'].rolling(window=window,center=True).mean()
     df['PriceUSD'] = df['PriceUSD'].rolling(window=window,center=True).mean()
@@ -21,4 +24,12 @@ def generate_global_time_series(file_path) -> pd.DataFrame:
     # Put date at first column
     date_col = df.pop('date')
     df.insert(0,'date',date_col)
+    return df
+
+def generate_blockchain_by_actor(file_path):
+    df = pd.read_csv(filepath_or_buffer=file_path)
+    df['date'] = pd.to_datetime(df['date']) 
+    # Unuseful columns
+    df = df.drop(['Unnamed: 0','year','day','month'],axis=1)
+    
     return df
