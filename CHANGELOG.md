54550f5ff9aefee335c509b02ca6f2513c225ed6
Bast-94 <bh.69@live.fr>
2023-04-10 02:09:31 +0200

    ADD(corr): Début d'analyse de corrélation des courbes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# v1.2.0
+- Add:
+    - Beginning of similarities computing between actors.
 
 # v1.1.0
 - Add:
--- scripts/data_frame_generator.py
+++ scripts/data_frame_generator.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 # Generate external time series.
 def generate_external_time_series(file_path, window=1) -> pd.DataFrame:
     # Load Dataframe 
@@ -10,6 +12,7 @@ def generate_external_time_series(file_path, window=1) -> pd.DataFrame:
     df['date'] = pd.to_datetime(df['date'])
     df['HashRate'] = df['HashRate'].rolling(window=window,center=True).mean()
     df['PriceUSD'] = df['PriceUSD'].rolling(window=window,center=True).mean()
+    df = df.set_index('date')
     return df
 
 def generate_global_time_series(file_path) -> pd.DataFrame:
@@ -24,6 +27,7 @@ def generate_global_time_series(file_path) -> pd.DataFrame:
     # Put date at first column
     date_col = df.pop('date')
     df.insert(0,'date',date_col)
+    df = df.set_index('date')
     return df
 
 def generate_blockchain_by_actor(file_path):
@@ -31,5 +35,17 @@ def generate_blockchain_by_actor(file_path):
     df['date'] = pd.to_datetime(df['date']) 
     # Unuseful columns
     df = df.drop(['Unnamed: 0','year','day','month'],axis=1)
-    
+    df = df.set_index('date')
     return df
+
+def min_max_norm(df):
+    min_max_scaler = MinMaxScaler()
+    df_normalized = min_max_scaler.fit_transform(df)
+    df_normalized = pd.DataFrame(np.array(df_normalized, dtype=np.float64), columns=df.columns)
+    return df_normalized
+
+def std_scale(df):
+    std_scaler = StandardScaler()
+    df_normalized = std_scaler.fit_transform(df)
+    df_normalized = pd.DataFrame(np.array(df_normalized, dtype=np.float64), columns=df.columns)
+    return df_normalized
