9802cd9591fa2c70392d8b9c59a519b9ce885be4
Bast-94 <bh.69@live.fr>
2023-04-11 11:51:16 +0200

    REFCRATOR(best_correlation.py): Déplacement des fonctions implémentées dans le notebook vers best_correlation.py.

--- scripts/best_correlation.py
+++ scripts/best_correlation.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import numpy as np
 
-def compute_best_correlation_by_col(block_chain_df: pd.DataFrame,column_to_analyze: str ) -> tuple:
-    block_chain_df.groupby('identity')[[column_to_analyze]].sum().sort_values(by=column_to_analyze, ascending=False).head()
-    short_blochain_df = block_chain_df.copy()
-    df_unstacked = short_blochain_df.groupby(['date', 'identity'])[column_to_analyze].mean().unstack()
-    actor_correlation_matrix = df_unstacked.corr()
-    np.fill_diagonal(actor_correlation_matrix.values,0)
+def get_best_values(actor_correlation_matrix: pd.DataFrame) -> tuple:
     s = actor_correlation_matrix.stack()
     max_index = s.idxmax()[0]
     max_col = actor_correlation_matrix.loc[max_index].idxmax()
     max_value = actor_correlation_matrix[max_col].loc[max_index]
     return max_index,max_col, max_value
 
+def compute_best_correlation_by_col(block_chain_df: pd.DataFrame,column_to_analyze: str ) -> tuple:
+    df_unstacked = block_chain_df.groupby(['date', 'identity'])[column_to_analyze].mean().unstack()
+    actor_correlation_matrix = df_unstacked.corr()
+    np.fill_diagonal(actor_correlation_matrix.values,0)
+    return get_best_values(actor_correlation_matrix)
+
 def compute_best_correlation(block_chain_df: pd.DataFrame):
     best_index, best_col,best_val = None,None, 0
-    for column_to_analyze in block_chain_df.columns:
+    best_col_to_analyse = None
+    for column_to_analyze in block_chain_df.columns[1:]:
         cur_index,cur_col,cur_val = compute_best_correlation_by_col(block_chain_df,column_to_analyze)
         if(cur_val>best_val):
             best_index, best_col,best_val =cur_index,cur_col,cur_val
+            best_col_to_analyse = column_to_analyze
     
-    return best_index, best_col,best_val
+    return best_index, best_col,best_val,best_col_to_analyse
 
